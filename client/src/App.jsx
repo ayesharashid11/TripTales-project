fb1a72ac017dfe42538bcd694b59c6751d3aae5c
Muffin1122 <bscs2211@gmail.com>
2024-05-27 07:36:02 +0500

    added browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./screens/LandingPage"
+import PageNotFound from "./screens/PageNotFound"
+import Header from "./components/Header"
+
 
 const App = () => {
   return (
-    <div>
-      hi
-    </div>
+   <BrowserRouter>
+   <Header />
+   <Routes>
+    <Route index element={<LandingPage/>}></Route>
+    <Route path="*" element={<PageNotFound />}></Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
