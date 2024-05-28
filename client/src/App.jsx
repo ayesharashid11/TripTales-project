a89331e6c36caae445f96cca58cf603fa7e44787
Muffin1122 <bscs2211@gmail.com>
2024-05-28 06:39:04 +0500

    SignIn and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from "./screens/LandingPage"
 import PageNotFound from "./screens/PageNotFound"
 import Header from "./components/Header"
-
-
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -13,6 +12,7 @@ const App = () => {
     <Route index element={<LandingPage/>}></Route>
     <Route path="*" element={<PageNotFound />}></Route>
    </Routes>
+   <Footer />
    </BrowserRouter>
   )
 }
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -8,12 +8,13 @@ const Signin = () => {
 
   return (
     <>
-      <Button className='bg-none' onClick={() => setOpenModal(true)}>Toggle modal</Button>
+      <Button className="text-white bg-emerald-700  rounded-lg text-sm px-4 py-1 text-center"
+      onClick={() => setOpenModal(true)}>Sign In</Button>
       <Modal show={openModal} size="md" popup onClose={() => setOpenModal(false)} >
         <Modal.Header />
         <Modal.Body>
           <div className="space-y-6">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+            <h3 className="text-xl font-medium text-gray-900">Sign in to our platform</h3>
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="email" value="Your email" />
@@ -31,16 +32,16 @@ const Signin = () => {
                 <Checkbox id="remember" />
                 <Label htmlFor="remember">Remember me</Label>
               </div>
-              <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
+              <a href="#" className="text-sm text-cyan-700 hover:underline">
                 Lost Password?
               </a>
             </div>
             <div className="w-full">
               <Button>Log in to your account</Button>
             </div>
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+            <div className="flex justify-between text-sm font-medium text-gray-500">
               Not registered?&nbsp;
-              <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
+              <a href="#" className="text-cyan-700 hover:underline">
                 Create account
               </a>
             </div>
