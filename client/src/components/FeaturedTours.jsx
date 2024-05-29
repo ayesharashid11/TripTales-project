12b5e656c2043670190c8f3928c99bda4b28fe21
Muffin1122 <bscs2211@gmail.com>
2024-05-29 06:42:36 +0500

    created trip activities slider

--- client/src/components/FeaturedTours.jsx
+++ client/src/components/FeaturedTours.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
+import { Card } from "flowbite-react";
 import { MdOutlineImportantDevices } from "react-icons/md";
+import { IoStarSharp } from "react-icons/io5";
+import scene3 from '../assets/scene3.jpg';
 const FeaturedTours = () => {
   return (
     <div className='p-7'>
        <h2 className="text-3xl flex items-center justify-center text-emerald-700 font-semibold mb-4">
             <MdOutlineImportantDevices className='text-4xl mr-3 text-yellow-500'/> Our Featured Tours 
             </h2>
+
+            <Card
+      className="max-w-sm"
+      imgAlt="  Starlight Sport"
+      imgSrc={scene3}
+    >
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+         Starlight Sport
+        </h5>
+      <div className="mb-5 mt-2.5 flex items-center">
+      <IoStarSharp/><IoStarSharp/><IoStarSharp/><IoStarSharp/>
+        <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-8000">
+          5.0
+        </span>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+        <a
+          href="#"
+          className="rounded-lg bg-emerald-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800  "
+        >
+          Add to cart
+        </a>
+      </div>
+    </Card>
     </div>
   )
 }
 
 export default FeaturedTours
+
+ 
--- client/src/components/TripActivities.jsx
+++ client/src/components/TripActivities.jsx
@@ -1,14 +1,72 @@
 import React from 'react'
 import { FiActivity } from "react-icons/fi";
+import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
+import { GiHiking } from "react-icons/gi";
+import { FaPersonSwimming } from "react-icons/fa6";
+import { GiCycling } from "react-icons/gi";
+import { GiBoatFishing } from "react-icons/gi";
+import { SiJusteat } from "react-icons/si";
+import '../screens/styles.css';
 
 const TripActivities = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   return (
-    <div >
-      <h2 className="text-3xl p-5 flex  text-emerald-700 font-semibold mb-4">
-            <FiActivity  className='text-4xl mr-3 text-yellow-500'/> Trip Activities 
-            </h2>
+    <div className="flex flex-col md:flex-row p-5">
+      <div className="md:w-1/4 mb-4 md:mb-0">
+        <h2 className="text-3xl flex items-center text-emerald-700 font-semibold mt-6">
+          <FiActivity className='text-4xl mr-3 text-yellow-500' /> Trip Activities
+        </h2>
+      </div>
+      <div className="md:w-3/4">
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          autoPlaySpeed={2000}
+          infinite={true}
+        >
+          <div className='flex flex-col items-center mx-2 p-4 w-[150px]'>
+            <GiHiking className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2' />
+            <p>Hiking</p>
+          </div>
+          <div className='flex flex-col items-center mx-2 p-4 w-[150px]'>
+            <FaPersonSwimming className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2' />
+            <p>Swimming</p>
+          </div>
+          <div className='flex flex-col items-center mx-2 p-4 w-[150px]'>
+            <GiCycling className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2' />
+            <p>Cycling</p>
+          </div>
+          <div className='flex flex-col items-center mx-2 p-4 w-[150px]'>
+            <GiBoatFishing className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2' />
+            <p>Fishing</p>
+          </div>
+          <div className='flex flex-col items-center mx-2 p-4 w-[150px]'>
+            <SiJusteat className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2' />
+            <p>Eat</p>
+          </div>
+        </Carousel>
+      </div>
     </div>
   )
 }
 
-export default TripActivities
+export default TripActivities;
