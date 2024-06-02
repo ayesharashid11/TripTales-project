2bf0ef8b98bb6ae4a63092d1bed0fa66fa11189f
Muffin1122 <bscs2211@gmail.com>
2024-06-02 18:35:20 +0500

    "Refactored TripActivities and Tours components, updating styles, icons, and layout."

--- client/src/components/TripActivities.jsx
+++ client/src/components/TripActivities.jsx
@@ -14,26 +14,30 @@ const TripActivities = () => {
         <FiActivity className='text-4xl mr-3 text-yellow-500' /> Trip Activities
       </h2>
       <div className=" flex flex-col md:flex-row m-6 mb-7">
-        <div className='items-center m-2 p-4 border-r-2 border-b-2 border-gray-300 shadow-lg rounded-lg w-56'>
-          <GiHiking className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2 hover:text-yellow-400' />
-          <h1 className='font-medium m-1'>Hiking</h1 >
-          <p>hike there with ease</p>
+        <div className='flex flex-col items-center justify-center m-2 p-4 border-r-2 border-b-2 hover:border-emerald-700 shadow-lg rounded-lg w-56 border-gray-300'>
+          <GiHiking className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2' />
+          <h1 className='font-medium m-1 text-center'>Hiking</h1>
+          <p>Hike through scenic trails and enjoy breathtaking views.</p>
         </div>
-        <div className='items-center m-2 p-4 border-r-2 border-b-2 border-gray-300 shadow-lg rounded-lg  w-56'>
-          <FaPersonSwimming className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2  ' />
-          <h1 className='font-medium m-1'>Swimming</h1>
+        <div className='flex flex-col items-center justify-center m-2 p-4 border-r-2 border-b-2 hover:border-emerald-700 shadow-lg rounded-lg w-56 border-gray-300'>
+          <FaPersonSwimming className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2'/>
+          <h1 className='font-medium m-1 text-center'>Swimming</h1>
+          <p>Dive into crystal-clear waters and enjoy refreshing swims.</p>
         </div>
-        <div className='items-center m-2 p-4 border-r-2 border-b-2 border-gray-300 shadow-lg rounded-lg w-56'>
-          <GiCycling className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2 ' />
-          <h1 className='font-medium m-1'>Cycling</h1>
+          <div className='flex flex-col items-center justify-center m-2 p-4 border-r-2 border-b-2 hover:border-emerald-700 shadow-lg rounded-lg w-56 border-gray-300'>
+          <GiCycling className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2'/>
+          <h1 className='font-medium m-1 text-center'>Cycling</h1>
+          <p>Ride through picturesque trails and enjoy the freedom of cycling.</p>
         </div>
-        <div className='items-center m-2 p-4  border-r-2 border-b-2 border-gray-300 shadow-lg rounded-lg w-56'>
-          <GiBoatFishing className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2 ' />
-          <h1 className='font-medium m-1'>Fishing</h1>
+        <div className='flex flex-col items-center justify-center m-2 p-4 border-r-2 border-b-2 hover:border-emerald-700 shadow-lg rounded-lg w-56 border-gray-300'>
+          <GiBoatFishing className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2' />
+          <h1 className='font-medium m-1 text-center'>Fishing</h1>
+          <p>Cast your line and catch a variety of fish in serene waters.</p>
         </div>
-        <div className='items-center m-2 p-4 border-r-2 border-b-2 border-gray-300 shadow-lg rounded-lg w-56'>
-          <SiJusteat className='text-6xl text-white rounded-full bg-emerald-700 p-3 mb-2 ' />
-          <h1 className='font-medium m-1'>Eat</h1>
+        <div className= 'flex flex-col items-center justify-center m-2 p-4 border-r-2 border-b-2 hover:border-emerald-700 shadow-lg rounded-lg w-56 border-gray-300'>
+          <SiJusteat className='text-6xl text-white flex items-center justify-center rounded-full bg-emerald-700 p-3 mb-2' />
+          <h1 className='font-medium m-1 text-center'>Eat</h1>
+          <p>Savor the flavors of local cuisine on our culinary tours.</p>
         </div>
       </div>
     </>
