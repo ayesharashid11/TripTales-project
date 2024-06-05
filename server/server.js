724059c0df30d5cb61c2e13ddfa8df954b400d93
Muffin1122 <bscs2211@gmail.com>
2024-06-05 15:42:12 +0500

    db connection

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+const app= express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+const connectDb = async () => {
+    mongoose.connect(process.env.MONGODB_CLOUD, )
+       .then(() => {
+         console.log(`Connection successful to ${mongoose.connection.host}`);
+       })
+       .catch(err => {
+         console.error(`Error connecting to database: ${err.message}`);
+       });
+   };
+   connectDb();
+
+const port = process.env.PORT || 8000;
+app.listen (port , ()=> {
+    console.log(`Server is running on ${port} !`);
+})
