396734de4e8c526c9f69bef2eb4ade480da473ac
Muffin1122 <bscs2211@gmail.com>
2024-06-05 17:34:02 +0500

    added auth controllers and routes

---
+++ server/models/userScehema.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    role: {
+        type: String,
+        enum: ['tuorist', 'company'],
+        default: 'tuorist'
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6,
+        select: false
+    },
+    confirmPassword: {
+        type: String,
+        require: true,
+    },
+    companyName: {
+        type: String,
+        required: function() {
+            return this.role === 'company';
+        }
+    },
+    country: {
+        type: String,
+        required: function() {
+            return this.role === 'company';
+        }
+    }
+});
+
+userSchema.pre('save', function(next) {
+    if (this.password !== this.confirmPassword) {
+        return next(new Error('Passwords do not match'));
+    }
+    next();
+});
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) 
+      return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+  });
+  
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRouter = require('./routes/authRoutes');
 dotenv.config();
 const app= express();
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api/auth', authRouter);
 
 const connectDb = async () => {
     mongoose.connect(process.env.MONGODB_CLOUD, )
