7e015662f707a7690e14426d6c1b326460515c1c
Muffin1122 <bscs2211@gmail.com>
2024-06-05 05:59:43 +0500

    Signup components with minor layout and styling changes, added header with link to homepage

--- client/src/screens/CreateTour.jsx
+++ client/src/screens/CreateTour.jsx
@@ -1,87 +1,3 @@
-// import React from 'react'
-// import mount2 from '../assets/mount2.jpg';
-// const CreateTour = () => {
-//     return (
-//         <div className=" relative h-screen w-full">
-//             <img src={mount2} className="h-full w-full object-cover" alt="Background" />
-//             <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-30">
-//                 <div className="bg-indigo-900 bg-opacity-60 p-8  rounded-lg shadow-lg w-full max-w-2xl">
-//                     <h1 className='text-md md:text-xl text-white font-medium'>Tour Details</h1>
-//                     <form className="space-y-5">
-//                         <div className="relative z-0 w-full group">
-//                             <div className="grid md:grid-cols-4 md:gap-6">
-//                                 <div className="relative z-0 w-full group">
-//                                     <input type="text" name="tour name" id="tour name" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-//                                     <label htmlFor="tour name" className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tour Name</label>
-//                                 </div>
-//                                 <div className="relative z-0 w-full group">
-//                                     <input type="text" name="country" id="country" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                         placeholder=" "
-//                                     />
-//                                     <label htmlFor="country" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Country</label>
-//                                 </div>
-//                                 <div className="relative z-0 w-full group">
-//                                     <input type="number" name="days" id="days" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                         placeholder=" "
-//                                     />
-//                                     <label htmlFor="days" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Total Days</label>
-//                                 </div>
-//                                 <div className="relative z-0 w-full group">
-//                                     <input type="number" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                         placeholder=" " required
-//                                     />
-//                                     <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className="grid md:grid-cols-3 md:gap-6">
-//                             <div className="relative z-0 w-full group">
-//                                 <input type="text" name="city" id="text" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                     placeholder=" " required
-//                                 />
-//                                 <label htmlFor="city" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">City</label>
-//                             </div>
-//                             <div className="relative z-0 w-full group">
-//                                 <input type="text" name="address" id="address" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-//                                 <label htmlFor="address" className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Departure Address</label>
-//                             </div>
-//                             <div className="relative z-0 w-full group">
-//                                 <input type="number" name="seats" id="seats" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                     placeholder=" " required
-//                                 />
-//                                 <label htmlFor="seats" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Total Seats</label>
-//                             </div>
-//                         </div>
-//                         <div className="relative z-0 w-full group">
-//                             <input type="file" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                 placeholder=" "
-//                             />
-//                         </div>
-//                         <h1 className='text-md md:text-xl text-white font-medium'>Contact Details</h1>
-//                         <div className="grid md:grid-cols-2 md:gap-6">
-//                             <div className="relative z-0 w-full group">
-//                                 <input type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-//                                     placeholder=" " required
-//                                 />
-//                                 <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
-//                             </div>
-//                             <div className="relative z-0 w-full group">
-//                                 <input type="tel" name="phone" id="phone" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-//                                 <label htmlFor="phone" className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone No.</label>
-//                             </div>
-//                         </div>
-//                         <button type="submit" className="text-white flex  bg-emerald-700 bg-opacity-70 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-//                             Create Tour
-//                         </button>
-//                     </form>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default CreateTour
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import mount2 from '../assets/mount2.jpg';
@@ -91,7 +7,7 @@ const CreateTour = () => {
         <div className="relative h-screen w-full">
             <img src={mount2} className="h-full w-full object-cover" alt="Background" />
             <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-30">
-                <header className="absolute top-0 left-0 p-4">
+                <header className="absolute top-0 left-0 p-5">
                     <Link to="/" className="text-white md:text-2xl font-semibold">TripTales</Link>
                 </header>
                 <div className="bg-indigo-900 bg-opacity-60 p-8 rounded-lg shadow-lg w-full max-w-2xl">
--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import mount1 from '../assets/mount1.jpg';
 import { useFormik } from "formik";
 import { signUpSchema } from "../schemas";
@@ -8,7 +9,7 @@ const initialValues = {
   email: "",
   password: "",
   confirm_password: "",
-  company:"",
+  company: "",
 };
 
 
@@ -34,6 +35,9 @@ const Signup = () => {
     <div className="relative h-screen w-full">
       <img src={mount1} className="h-full w-full object-cover" alt="Background" />
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-20">
+        <header className="absolute top-0 left-0 p-5">
+          <Link to="/" className="text-white md:text-2xl font-semibold">TripTales</Link>
+        </header>
         <div className="bg-indigo-900 bg-opacity-60 p-8  rounded-lg shadow-lg w-full max-w-md">
           <div className="flex justify-center mb-6">
             <label className="mr-4 text-white text-lg font-medium">
@@ -67,28 +71,28 @@ const Signup = () => {
                     type="text"
                     name="name"
                     id="name"
-                    className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " 
+                    className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "
                     // required
                     value={values.name}
                     onChange={handleChange}
                     onBlur={handleBlur}
                   />
-                   {errors.name && touched.name ? (
-                      <p className="text-red-400 text-sm">{errors.name}</p>
-                    ) : null}
+                  {errors.name && touched.name ? (
+                    <p className="text-red-400 text-sm">{errors.name}</p>
+                  ) : null}
                   <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                     Name
                   </label>
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                   <div className="relative z-0 w-full group">
-                    <input type="text" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                    placeholder=" " 
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+                    <input type="text" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                       {errors.email && touched.email ? (
+                    {errors.email && touched.email ? (
                       <p className="text-red-400 text-sm">{errors.email}</p>
                     ) : null}
                     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
@@ -96,13 +100,13 @@ const Signup = () => {
                     </label>
                   </div>
                   <div className="relative z-0 w-full group">
-                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                    placeholder=" " 
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                       {errors.password && touched.password ? (
+                    {errors.password && touched.password ? (
                       <p className="text-red-400 text-sm">{errors.password}</p>
                     ) : null}
                     <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
@@ -114,13 +118,13 @@ const Signup = () => {
               <>
                 <div className="relative z-0 w-full group">
                   <div className="relative z-0 w-full group">
-                    <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:ring-0 focus:border-blue-600 peer" 
-                    placeholder=" " required 
-                    value={values.name}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+                    <input type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" " required
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                       {errors.name && touched.name ? (
+                    {errors.name && touched.name ? (
                       <p className="text-red-400 text-sm">{errors.name}</p>
                     ) : null}
                     <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
@@ -128,55 +132,55 @@ const Signup = () => {
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                   <div className="relative z-0 w-full group">
-                    <input type="text"  name="country" id="country" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" name="country" id="country" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="country" className="peer-focus:font-medium absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Country</label>
                   </div>
                   <div className="relative z-0 w-full group">
-                    <input type="text" name="company" id="company" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                    placeholder=" "
-                    value={values.company}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+                    <input type="text" name="company" id="company" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" "
+                      value={values.company}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                       {errors.company && touched.company ? (
+                    {errors.company && touched.company ? (
                       <p className="text-red-400 text-sm">{errors.company}</p>
                     ) : null}
                     <label htmlFor="company" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company Name</label>
                   </div>
                 </div>
                 <div className="relative z-0 w-full group">
-                  <input type="text" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                  placeholder=" " 
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
+                  <input type="text" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    placeholder=" "
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
                   />
-                     {errors.email && touched.email ? (
-                      <p className="text-red-400 text-sm">{errors.email}</p>
-                    ) : null}
+                  {errors.email && touched.email ? (
+                    <p className="text-red-400 text-sm">{errors.email}</p>
+                  ) : null}
                   <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                   <div className="relative z-0 w-full group">
-                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                    placeholder=" " required 
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
+                    <input type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                      placeholder=" " required
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                       {errors.password && touched.password ? (
+                    {errors.password && touched.password ? (
                       <p className="text-red-400 text-sm">{errors.password}</p>
                     ) : null}
                     <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                   </div>
                   <div className="relative z-0 w-full group">
                     <input type="password" name="confirm_password" id="confirm_password" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                     placeholder=" " required 
-                     value={values.confirm_password}
-                     onChange={handleChange}
-                     onBlur={handleBlur}
-                     />
-                        {errors.confirm_password && touched.confirm_password ? (
+                      placeholder=" " required
+                      value={values.confirm_password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.confirm_password && touched.confirm_password ? (
                       <p className="text-red-400 text-sm">{errors.confirm_password}</p>
                     ) : null}
                     <label htmlFor="confirm_password" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm Password</label>
