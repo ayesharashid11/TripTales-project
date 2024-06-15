6e07f3fcaaa68e06f7789f637ac18ea7804c68ab
Muffin1122 <bscs2211@gmail.com>
2024-06-15 23:48:18 +0500

    controller changed

--- server/payment-integration/paymentController.js
+++ server/payment-integration/paymentController.js
@@ -1,23 +1,23 @@
 const JC = require('./jazzcash');
 
 exports.pay = (req, res) => {
-  const { amount, description, billReference } = req.body;
+  const { amount, description, mobileNumber, cnic } = req.body;
 
   const data = {
     pp_Version: '1.1',
-    pp_DiscountedAmount: '',
-    pp_DiscountBank: '',
-    pp_Amount: amount,
-    pp_TxnCurrency: 'PKR',
-    pp_BillReference: billReference,
+    pp_Amount: amount * 100, // Convert to smallest currency unit
     pp_Description: description,
+    pp_MobileNumber: mobileNumber,
+    pp_CNIC: cnic,
+    pp_TxnCurrency: 'PKR',
+    pp_BillReference: 'billRef123',
   };
 
   JC.pay(data, response => {
     if (response.pp_SecureHash) {
-      res.status(200).json({  data: response });
+      res.status(200).json({ success: true, data: response });
     } else {
-      res.status(500).json({ message: 'Payment failed', data: response });
+      res.status(500).json({ success: false, message: 'Payment failed', data: response });
     }
   });
 };
